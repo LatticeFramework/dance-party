14c58a81ad31d7723e1fe00ad259f5e462014f32
LatticeFramework <10vv3cl@gmail.com>
2023-05-07 21:26:29 -0400

    Link external JS in body to follow DOM, and create basic function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello Jim");
+
+// const i = 0;
+// const j = 0;
+
+// const divTag = document.querySelector(".dance-square");
+// const bgColor = ["purple", "yellow", "blue", "brown"]
+
+// function divChange() {
+//     divTag.getElementsByClassName.backgroundColor = blue;
+//     i = (i + 1) % bgColor.length;
+// }
+
+// setInterval(divChange, 2000);
+
+function changeStyle() {
+    const squares = document.getElementById("square-one");
+    squares.style.backgroundColor = "#00FF00";
+}
+
+changeStyle();
